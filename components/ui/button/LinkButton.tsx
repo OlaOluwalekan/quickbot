d2e85cd19a3aceb8b3c8b91140ff05df7c364484
OlaOluwalekan <olalekanbello534@gmail.com>
2024-07-25 20:31:16 +0100

    rectify another error

--- components/ui/button/LinkButton.tsx
+++ components/ui/button/LinkButton.tsx
@@ -1,17 +1,21 @@
-import { LinkBtnProps } from '@/types'
-import clsx from 'clsx'
-import Link from 'next/link'
-import { sizeClass, themeClass } from './button.config'
+import { LinkBtnProps } from "@/types";
+import clsx from "clsx";
+import Link from "next/link";
+import { sizeClass, themeClass } from "./button.config";
 
 const LinkButton = ({ size, text, theme, href }: LinkBtnProps) => {
   return (
     <Link
       href={href}
-      className={clsx(sizeClass(size), themeClass(theme), 'btn py-2 rounded')}
+      className={clsx(
+        sizeClass(size),
+        themeClass(theme as "base" | "primary" | "outline"),
+        "btn py-2 rounded"
+      )}
     >
       {text}
     </Link>
-  )
-}
+  );
+};
 
-export default LinkButton
+export default LinkButton;
