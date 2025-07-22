dae7007b299e2f1b64c5d40422b67d59685e3f23
OlaOluwalekan <olalekanbello534@gmail.com>
2025-07-22 16:39:23 +0100

    refactor: updated tailwind css for settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,41 +1,41 @@
-import { auth } from "@/auth";
-import Logout from "@/components/auth/Logout";
-import NameAndEmail from "@/components/settings/NameAndEmail";
-import SettingsCard from "@/components/settings/SettingsCard";
-import TokensDisplay from "@/components/settings/TokensDisplay";
-import UserProfileImage from "@/components/settings/UserProfileImage";
-import LinkButton from "@/components/ui/button/LinkButton";
-import { SessionProps } from "@/types/user";
+import { auth } from '@/auth'
+import Logout from '@/components/auth/Logout'
+import NameAndEmail from '@/components/settings/NameAndEmail'
+import SettingsCard from '@/components/settings/SettingsCard'
+import TokensDisplay from '@/components/settings/TokensDisplay'
+import UserProfileImage from '@/components/settings/UserProfileImage'
+import LinkButton from '@/components/ui/button/LinkButton'
+import { SessionProps } from '@/types/user'
 
 const SettingsPage = async () => {
   // get authenticated user session
-  const session = await auth();
+  const session = await auth()
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className='flex justify-center items-center h-screen'>
       <SettingsCard>
-        <div className="w-full flex flex-col md:flex-row md:justify-start md:items-center">
-          <div className="flex flex-col justify-center items-center">
+        <div className='w-full flex flex-col md:flex-row md:justify-start md:items-center'>
+          <div className='flex flex-col justify-center items-center'>
             <UserProfileImage user={session?.user as SessionProps} />
             <NameAndEmail user={session?.user as SessionProps} />
           </div>
-          <div className="divider md:divider-horizontal"></div>
+          <div className='divider md:divider-horizontal'></div>
           <div>
             <TokensDisplay user={session?.user as SessionProps} />
           </div>
         </div>
-        <article className="w-full flex justify-end mt-3 gap-2">
+        <article className='w-full flex justify-end mt-3 gap-2'>
           <LinkButton
-            href="/chat"
-            size="medium"
-            theme="primary"
-            text="Go to Chat"
+            href='/chat'
+            size='medium'
+            theme='primary'
+            text='Go to Chat'
           />
-          <Logout styleClass="w-fit px-5 py-3 bg-error/30 text-error hover:bg-error hover:text-white" />
+          <Logout styleClass='w-fit px-5 py-3 bg-error/30 text-error hover:bg-error hover:text-white cursor-pointer' />
         </article>
       </SettingsCard>
     </div>
-  );
-};
+  )
+}
 
-export default SettingsPage;
+export default SettingsPage
--- components/settings/UserProfileImage.tsx
+++ components/settings/UserProfileImage.tsx
@@ -1,23 +1,23 @@
-import { SessionProps } from "@/types/user";
-import Image from "next/image";
+import { SessionProps } from '@/types/user'
+import Image from 'next/image'
 
 const UserProfileImage = ({ user }: { user: SessionProps }) => {
   return (
-    <div className="rounded-full border-2 border-primary overflow-hidden w-20 aspect-square flex justify-center items-center">
+    <div className='rounded-full border-2 border-primary dark:border-dark-primary overflow-hidden w-20 aspect-square flex justify-center items-center'>
       {user.image ? (
         <Image
           src={user.image as string}
-          alt="Profile Image"
+          alt='Profile Image'
           width={100}
           height={100}
-          className="w-full aspect-square"
+          className='w-full aspect-square'
         />
       ) : (
-        <article className="text-6xl font-bold">
+        <article className='text-5xl font-bold'>
           {user.name.charAt(0).toUpperCase()}
         </article>
       )}
     </div>
-  );
-};
-export default UserProfileImage;
+  )
+}
+export default UserProfileImage
